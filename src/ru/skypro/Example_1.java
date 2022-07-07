2e393c1d78bb6adefa72057467c1823c4cc5594e
Dmitriy <shadowgods@mail.ru>
2022-07-07 18:53:07 +0500

    add two strokes

--- src/ru/skypro/Example_1.java
+++ src/ru/skypro/Example_1.java
@@ -3,5 +3,7 @@ package ru.skypro;
 public class Example_1 {
     public static void main(String[] args) {
         System.out.println("Первый урок");
+        System.out.println("ура");
+        System.out.println("идем дальше");
     }
 }
